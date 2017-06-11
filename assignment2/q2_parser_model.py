6b95f46b34c425aaa69890dc43f6de2b3e973bef
madalinabuzau <buzaumadalina@gmail.com>
2017-06-11 17:29:11 +0100

    Added placeholders to ParserModel

--- assignment2/q2_parser_model.py
+++ assignment2/q2_parser_model.py
@@ -54,6 +54,11 @@ class ParserModel(Model):
         (Don't change the variable names)
         """
         ### YOUR CODE HERE
+        self.input_placeholder = tf.placeholder(tf.int32, 
+                                                shape=(None, self.config.n_features))
+        self.labels_placeholder = tf.placeholder(tf.float32, 
+                                                 shape=(None, self.config.n_classes))
+        self.dropout_placeholder = tf.placeholder(tf.float32, shape=(1,))
         ### END YOUR CODE
 
     def create_feed_dict(self, inputs_batch, labels_batch=None, dropout=1):
